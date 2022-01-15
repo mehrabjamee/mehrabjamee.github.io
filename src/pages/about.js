5019a74b32ec81fc32c00e7fbd8838712e6c2869
mehrabjamee <jameemehrab@gmail.com>
2022-01-15 04:49:48 -0500

    Added layout, about page, and basic text

---
+++ src/pages/about.js
@@ -0,0 +1,15 @@
+import * as React from "react";
+import Layout from "../components/layout";
+
+const AboutPage = () => {
+  return (
+    <Layout pageTitle="About Me">
+      <p>
+        Hi there! This is Mehrab Jamee, a sophomore at MIT who wants to promote
+        himself.
+      </p>
+    </Layout>
+  );
+};
+
+export default AboutPage;
